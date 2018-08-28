0d76f20fc00fe452f2723b3b8981ea52c42be3b5
Jacking Zuo <676286266@qq.com>
2018-08-28 16:03:57 +0800

    在HelloWorld中添加SmartGit操作的相关说明

--- Log4jTest/src/HelloWorld.java
+++ Log4jTest/src/HelloWorld.java
@@ -4,6 +4,16 @@
  */
 public class HelloWorld {
     public static void main(String[] args) {
+
         System.out.println("HelloWorld");
+
+        //搭配SmartGit使用，实现本地仓库、远程仓库的同步
+
+        /**
+         * 1.  在Git中，git add和 git  stage作用是一样的，都是添加到暂存区
+         * 2.  SmartGit合并分支的操作：第一步：
+         *
+         * 3. 在push到远程之前要先从远程pull最新版本；在push之前，对照自己改动的版本进行审查
+         */
     }
 }
